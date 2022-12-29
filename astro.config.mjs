91900110216883e1afbe2f39311ab8ab608099a4
BoJoNVi <bonjomontes@gmail.com>
2022-12-29 14:05:56 +0800

    Implemented imagetools in tourist spot POI

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
     tailwind(),
     robotsTxt({ sitemapBaseFileName: "sitemap-0" }),
     sitemap(),
+    astroImageTools,
     compress({
       exclude: [
         (file) => {
@@ -23,7 +24,6 @@ export default defineConfig({
         },
       ],
     }),
-    astroImageTools,
   ],
   server: {
     port: 3000,
