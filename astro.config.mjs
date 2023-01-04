205c6f99532bc32618b963477197081a09f0df35
BoJoNVi <bonjomontes@gmail.com>
2023-01-04 10:01:12 +0800

    Removed any traces of react in the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,6 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://davao.page",
   integrations: [
