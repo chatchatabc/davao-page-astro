0074f7d3424bc520870af3c2550683c39b838789
BoJoNVi <bonjomontes@gmail.com>
2023-01-23 14:11:16 +0800

    chore: cleaned up some codes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://davao.page",
   integrations: [
