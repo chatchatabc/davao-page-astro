6794fb8f81387afe633a8b77f6b908fcac4a8cb6
BoJoNVi <bonjomontes@gmail.com>
2023-02-22 08:59:10 +0800

    build: added support for MDX content

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,20 @@ import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://davao.page",
-  integrations: [
-    tailwind(),
-    robotsTxt({ sitemapBaseFileName: "sitemap-0" }),
-    sitemap(),
-    compress({
-      exclude: [
-        (file) => {
-          if (file.includes("./dist/images/")) return file;
-        },
-      ],
-    }),
-    astroImageTools,
-  ],
+  integrations: [tailwind(), robotsTxt({
+    sitemapBaseFileName: "sitemap-0"
+  }), sitemap(), compress({
+    exclude: [file => {
+      if (file.includes("./dist/images/")) return file;
+    }]
+  }), astroImageTools, mdx()],
   server: {
     port: 3000,
-    host: false,
-  },
+    host: false
+  }
 });
