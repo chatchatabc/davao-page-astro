54f75c812932f26399b65e07f564209e153ef94d
BoJoNVi <bonjomontes@gmail.com>
2023-03-09 12:16:48 +0800

    build: removed astro-compress from build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
-import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -15,13 +14,6 @@ export default defineConfig({
       sitemapBaseFileName: "sitemap-0",
     }),
     sitemap(),
-    compress({
-      exclude: [
-        (file) => {
-          if (file.includes("./dist/images/")) return file;
-        },
-      ],
-    }),
     astroImageTools,
     mdx(),
   ],
